54102cacb5c282d0fbf926aebf098f286a0f55e8
Angie Vivas <angievivas1107@gmail.com>
2024-10-03 14:51:09 -0500

    se crea el proyecto con el archivo main y una funcion

---
+++ funcionesProject/Main.py
@@ -0,0 +1,12 @@
+#definicion de una funcion
+def my_function():
+    print("hello from a function")
+
+print("es hoy es hoy es hoy EL QUIZ")
+#invocar la funcion
+my_function()
+
+def mostrarnombre(nombre):
+    print("su nombre es " + nombre)
+nom = input("Ingrese su nombre: ")
+mostrarnombre(nom)
